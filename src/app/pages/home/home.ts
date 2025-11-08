8f8c67870663c748f35894de85cde956703e4741
james <james@cypressbayoumedia.com>
2025-11-08 15:00:12 +0000

    Angular Material Added

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -7,9 +7,13 @@ import { InviteMembers } from '../../components/invite-members/invite-members';
 import { Families } from '../../core/families';
 import { FamilySwitcher } from '../../components/family-switcher/family-switcher';
 import { CommonModule } from '@angular/common';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-home',
-  imports: [RouterModule,PostList, Landing, FamilySwitcher,InviteMembers, CommonModule],
+  imports: [RouterModule,PostList, Landing, FamilySwitcher,InviteMembers, CommonModule, MatMenuModule, MatIconModule],
+  standalone: true,
+  providers: [AuthService, Families],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
@@ -18,21 +22,22 @@ export class Home {
   public familiesService = inject(Families);
   
   // --- SIGNALS to control the UI ---
-  isUserMenuOpen = signal(false);
+  
   isInviteModalOpen = signal(false);
 
-  toggleUserMenu(): void {
-    this.isUserMenuOpen.update(value => !value);
-  }
+ 
   
   openInviteModal(): void {
     // We'll close the user menu if it's open when we open the invite modal
-    this.isUserMenuOpen.set(false);
+   
     this.isInviteModalOpen.set(true);
   }
   
   closeInviteModal(): void {
     this.isInviteModalOpen.set(false);
   }
+  switchFamily(familyId:string){
+    this.authService.switchActiveFamily(familyId);
 
+  }
 }
