53ff1e7d1a90a4d6496a05b7cb5611d62c973e5a
james <james@cypressbayoumedia.com>
2025-11-12 23:05:37 +0000

    Home Page Updates

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,15 +3,17 @@ import { RouterModule } from '@angular/router';
 import { PostList } from '../../posts/post-list/post-list';
 import { Landing } from '../landing/landing';
 import { AuthService } from '../../core/auth';
-import { InviteMembers } from '../../components/invite-members/invite-members';
 import { Families } from '../../core/families';
-import { FamilySwitcher } from '../../components/family-switcher/family-switcher';
 import { CommonModule } from '@angular/common';
 import {MatMenuModule} from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button'; 
+import { Calendar } from '../../core/calendar';
+import { map, Observable } from 'rxjs';
 @Component({
   selector: 'app-home',
-  imports: [RouterModule,PostList, Landing, FamilySwitcher,InviteMembers, CommonModule, MatMenuModule, MatIconModule],
+  imports: [RouterModule,PostList, Landing, CommonModule, MatMenuModule, MatIconModule, MatBadgeModule, MatButtonModule],
   standalone: true,
   providers: [AuthService, Families],
   templateUrl: './home.html',
@@ -20,10 +22,17 @@ import { MatIconModule } from '@angular/material/icon';
 export class Home {
   public authService = inject(AuthService);
   public familiesService = inject(Families);
-  
+  private calendarService = inject(Calendar);
+
+  public upcomingEventsCount$: Observable<number>;
 
   switchFamily(familyId:string){
     this.authService.switchActiveFamily(familyId);
 
+  }
+  constructor() {
+    this.upcomingEventsCount$ = this.calendarService.getUpcomingEvents().pipe(
+      map(events => events.length)
+    );
   }
 }
